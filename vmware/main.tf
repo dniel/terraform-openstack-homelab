f20806a1b50b988137769caf6ed9a113d5d1ee05
Daniel Nord <me@danielnord.no>
2025-11-05 19:18:39 +0100

    remove foo vm

--- vmware/main.tf
+++ vmware/main.tf
@@ -21,54 +21,6 @@ data "vsphere_content_library" "library" {
   name = var.content_library_name
 }
 
-data "vsphere_content_library_item" "template_debian" {
-  name       = var.content_library_item_name
-  type       = var.content_library_item_type
-  library_id = data.vsphere_content_library.library.id
-}
-
-resource "vsphere_virtual_machine" "vm_foo" {
-  name             = var.vm_name
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  clone {
-    template_uuid = data.vsphere_content_library_item.template_debian.id
-  }
-
-  num_cpus = var.vm_num_cpus
-  memory   = var.vm_memory
-
-  wait_for_guest_ip_timeout   = 0
-  wait_for_guest_net_timeout  = 0
-  wait_for_guest_net_routable = false
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label            = var.vm_disk_label
-    size             = var.vm_disk_size
-    unit_number      = 0
-    eagerly_scrub    = false
-    thin_provisioned = true
-  }
-
-  vapp {
-    properties = {
-      "guestinfo.hostname"  = var.guestinfo_hostname,
-      "guestinfo.ipaddress" = var.guestinfo_ipaddress,
-      "guestinfo.netprefix" = var.guestinfo_netprefix,
-      "guestinfo.gateway"   = var.guestinfo_gateway,
-      "guestinfo.dns"       = var.guestinfo_dns,
-      "guestinfo.domain"    = var.guestinfo_domain,
-      "guestinfo.password"  = var.guestinfo_password,
-      "guestinfo.sshkey"    = var.guestinfo_sshkey
-    }
-  }
-}
-
 data "vsphere_content_library_item" "template_ubuntu" {
   name       = "ubuntu-25.10-server"
   type       = var.content_library_item_type
